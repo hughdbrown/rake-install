19fc4ef3d71d3215a860dbea720abe733a4898e8
Hugh Brown <hbrown@wgen.net>
2013-10-27 23:41:03 -0400

    First steps

---
+++ first-steps.sh
@@ -0,0 +1,65 @@
+#!/bin/sh -e
+
+# rake git:bin
+# rake vim:vim
+# rake zsh:install
+
+# rake Admin                   # Install admin machine
+# rake Desktop                 # Install desktop machine
+# rake Developer               # Install developer machine
+# rake Server                  # Install server machine
+# rake Webdev                  # Install web developer machine
+# rake chrome:install          # Install google chrome
+# rake dev:essential           # Install essential tools
+# rake dev:language            # Install language tools
+# rake dev:libbz2              # Install libbz2 (a compression library)
+# rake dev:parallel            # Install parallel (for running tasks in parallel)
+# rake dev:tools               # Install dev tools
+# rake dev:web                 # Install web tools
+# rake dev:zip                 # Install zip tools
+# rake dropbox:service         # Install dropbox service
+# rake ipython:ipython         # Install ipython binary
+# rake java:java               # Install java
+# rake media:abcde             # Install abcde (for ripping CDs)
+# rake media:adobe             # Install Adobe plugins
+## rake media:adobe_reader      # Install Adobe reader
+# rake media:brasero           # Install brasero (for ripping DVDs)
+# rake media:handbrake         # Install handbrake (a client for creating digital video)
+# rake media:hosts             # Install hosts file
+# rake media:irssi             # Install irssi (a n IRC client)
+# rake media:kazam             # Install screenkey (for recording screen captures)
+## rake media:medibuntu         # Fix medibuntu
+rake media:pidgin            # Install pidgin (an IM client)
+rake media:screenkey         # Install screenkey (for screen demos)
+rake media:youtube_dl        # Install youtube_dl (fordownloading from youtube)
+rake mongo:bin               # Install mongo binaries
+rake mongo:mongo             # Install mongo and tools
+rake nginx:nginx             # Install nginx binary (with mod_wsgi and pagespeed)
+rake nginx:service           # Install nginx service
+rake node:bin                # Install node binary
+rake python:cpython          # Install python binary
+rake python:pip              # Install pip
+rake python:pypy             # Install pypy binary
+rake rabbitmq:rabbitmq       # Install rabbitmq library
+rake redis:bin               # Install redis database
+rake ruby:install            # Install ruby
+rake rubygems:install        # Install ruby gems
+rake rubygems:ruby_dev       # Install ruby-dev prerequisite
+rake schwab:icaclient        # Install icaclient and dependencies
+rake ssh:open_ssh_server     # Install openssh-server
+rake system:file_systems     # Install file systems
+rake system:libnotify        # Install libnotify
+rake tmux:bin                # Install tmux (terminal multiplexer)
+rake tmux:libevent           # Install libevent
+rake vagrant:vagrant         # Install vagrant
+# rake vim:vim_spf13           # Install vim-spf13
+rake virtualbox              # Install virtualbox
+rake virtualbox:virtualbox   # Install virtualbox
+rake virtualenv:hub          # Install hub (a github fronend for git)
+rake virtualenv:virtualenv   # Install virtualenv tools
+rake web:xvfb                # Install xvfb
+rake zsh:install             # Install zsh (a command shell)
+rake docs:latex              # Install latex
+rake docs:pandoc             # Install pandocs (package for all text formatting)
+rake sanescan:sanescan       # Install sane library for scanners
+
